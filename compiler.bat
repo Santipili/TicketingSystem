b6e7770d2620052c7c5621c6b56a028164c2cf32
Santipili <santi_mdq@live.com.ar>
2024-10-29 21:03:51 -0300

    add compiler and class diagram

---
+++ compiler.bat
@@ -0,0 +1,21 @@
+@echo off
+echo Compilando el programa...
+
+:: Intentar compilar el código
+g++ ./src/Ticket.cpp ./src/MessageSender.cpp main.cpp  -o TicketingSystem.exe -lstdc++
+
+:: Verificar si la compilación fue exitosa
+if %errorlevel% neq 0 (
+    echo Error en la compilacion. Por favor, revise el codigo.
+    pause
+    exit /b %errorlevel%
+)
+
+echo Compilacion exitosa.
+echo Ejecutando el programa...
+
+:: Ejecutar el programa
+TicketingSystem.exe
+
+:: Pausa para mantener la ventana abierta
+pause
